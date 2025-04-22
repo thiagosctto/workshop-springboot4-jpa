26e161060c710ed045b795ad919fa6a135ee063d
thiagosctto <tiago.games1000@gmail.com>
2025-04-22 20:31:10 -0300

    Tratamento de exceção - update

--- src/main/java/com/ts/demo/services/UserService.java
+++ src/main/java/com/ts/demo/services/UserService.java
@@ -13,6 +13,8 @@ import com.ts.demo.repositories.UserRepository;
 import com.ts.demo.services.exceptions.DatabaseException;
 import com.ts.demo.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,9 +47,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity,obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity,obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 
